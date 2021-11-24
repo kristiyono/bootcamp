5c698fb9568fe271404a4519d330b9938f3665ab
David Kristiyono <52364589+kristiyono@users.noreply.github.com>
2021-11-24 14:49:27 +0700

    Add files via upload

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,8 +1,38 @@
 <?php 
 
+require_once "../BusnissLogic/ShowTodoList.php";
+require_once "../BusnissLogic/AddTodoList.php";
+require_once "../BusnissLogic/RemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 function ViewShowTodoList()
 {
-    # code...
+    while (true) {
+        # code...
+        ShowTodoList();
+
+        echo "Menu". PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("pilih");
+
+        if ($pilihan == "1") {
+            # code...
+            ViewAddTodoList();
+        }elseif ($pilihan == "2") {
+            # code...
+            ViewRemoveTodoList();
+        }elseif ($pilihan == "x") {
+            # code...
+            // keluar
+        }else {
+            echo "Pilihan Tidak Mengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Kembali";
 }
 
 ?>
